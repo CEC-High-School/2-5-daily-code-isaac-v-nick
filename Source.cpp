28e6ef7bc2493509a1e5d84543603ec241f264e3
isaac <35504057+RedHood133@users.noreply.github.com>
2018-02-05 12:48:32 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double a;
+		double input;
+		cout << "what is one side of a cube" << endl;
+		cin >> input;
+		cout << 6 * input + 6 * input << endl;
+		cout << input*input*input << endl;
+
+
+
+
+
+
+}
